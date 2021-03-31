4ee2db8bbb9a75366b8a88c60b7b11d48ae1eba7
Flamur_Morina <fm41566@ubt-uni.net>
2021-03-31 23:08:14 +0200

    make link to css,js and image

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
