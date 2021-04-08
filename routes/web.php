ac817df6407973a11e54204e673085d808b05a83
Ahmed Kralani <ak42549@ubt-uni.net>
2021-04-08 19:15:50 +0200

    Create Doctor Form
    Signed-off-by: Ahmed Kralani <ak42549@ubt-uni.net>

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 });
-
-
-
-
-
-
 Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::resource('doctor', 'DoctorController');
+Route::resource('doctor', 'App\Http\Controllers\DoctorController');
