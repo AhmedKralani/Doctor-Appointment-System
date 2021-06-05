1b0aee19b1330b00f71933754f7fe96cceb1693d
Ahmed Kralani <ak42549@ubt-uni.net>
2021-06-06 00:43:03 +0200

    redirect doctor to dashboard
    Signed-off-by: Ahmed Kralani <ak42549@ubt-uni.net>

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        if( Auth::user()->role->name=="admin"){ 
+        if (Auth::user()->role->name=="admin" || Auth::user()->role->name="doctor") { 
             return redirect()->to('/dashboard');
         }
         return view('home');
