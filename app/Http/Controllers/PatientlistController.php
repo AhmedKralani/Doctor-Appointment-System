d354665081928eb6b569a3f2d7495398800fb8ff
Flamur_Morina <fm41566@ubt-uni.net>
2021-06-16 13:35:42 +0200

    filter patients by date

--- app/Http/Controllers/PatientlistController.php
+++ app/Http/Controllers/PatientlistController.php
@@ -8,9 +8,14 @@ use App\Models\Booking;
 
 class PatientlistController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         date_default_timezone_set('Europe/Skopje');
+        if($request->date){
+        $bookings = Booking::latest()->where('date',$request->date)->get();
+        return view('admin.patientlist.index',compact('bookings'));
+        }
+        
         $bookings = Booking::latest()->where('date',date('d-m-Y'))->get();
         return view('admin.patientlist.index',compact('bookings'));
     }
--- resources/views/admin/patientlist/index.blade.php
+++ resources/views/admin/patientlist/index.blade.php
@@ -5,15 +5,38 @@
     <div class="row justify-content-center">
         <div class="col-md-10">
             <div class="card">
-                <div class="card-header"> Appointments({{$bookings->count()}})</div>
+                <div class="card-header"> 
+                Appointments({{$bookings->count()}})
+                </div>
+                <form action="{{route('patient')}}" method="GET">
+
+                <div class="card-header"> 
+                 Filter:
+                 <div class="row">
+                 <div class="col-md-10">
+                 <input type="text" class="form-control datetimepicker-input" 
+                 id="datepicker" data-toggle="datetimepicker" 
+                 data-target="#datepicker" name="date">
+                 </div>
+                 <div class="col-md-2">
+                    <button type="submit" class="btn btn-primary">Search</button>
+                 </div>
+                </div>
+                </form>
+                </div>
+        
 
                 <div class="card-body">
                 <table class="table table-striped">
                       <thead>
                         <tr>
                           <th scope="col">#</th>
+                          <th scope="col">Photo</th>
                           <th scope="col">Date</th>
                           <th scope="col">User</th>
+                          <th scope="col">Email</th>
+                          <th scope="col">Phone</th>
+                          <th scope="col">Gender</th>
                           <th scope="col">Time</th>
                           <th scope="col">Doctor</th>
                           <th scope="col">Status</th>
@@ -23,8 +46,13 @@
                         @forelse($bookings as $key=>$booking)
                             <tr>
                                 <th scope="row">{{$key+1}}</th>
-                                <td>{{$booking->date}}</td>
+                                <td><img src="profile/{{$booking->user->image}}" width="80" style="border-radius: 50%;">
+                                </td>
+                                <td>{{$booking->date}}</td> 
                                 <td>{{$booking->user->name}}</td>
+                                <td>{{$booking->user->email}}</td>
+                                <td>{{$booking->user->phone_number}}</td>
+                                <td>{{$booking->user->gender}}</td>
                                 <td>{{$booking->time}}</td>
                                 <td>{{$booking->doctor->name}}</td>
                                 <td>
