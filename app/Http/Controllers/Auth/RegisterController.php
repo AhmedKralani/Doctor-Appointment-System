78ec1f53887117db422aa899d632ca9d983ca798
AhmedKralani <52008555+AhmedKralani@users.noreply.github.com>
2021-06-27 05:06:10 +0200

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        $role = Role::where('name','admin')->first();
+        $role = Role::where('name','patient')->first();
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
